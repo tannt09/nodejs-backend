968d4563c657106710c36256849448fbcc13a73b
Nguyen Tan <vantankmt@gmail.com>
2024-08-29 10:21:51 +0700

    feat: create product update api

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -11,6 +11,22 @@ class ProductsController {
       }
     });
   }
+
+  async update(req, res, ___) {
+    const productsId = req.query.id;
+    const newOriginPrice = req.body.price;
+
+    client.query("UPDATE products SET original_price = $1 WHERE id = $2", [newOriginPrice, productsId],
+      (err, result) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send("Error updating data");
+        } else {
+          res.send("Price updated successfully")
+        }
+      }
+    )
+  }
 }
 
 module.exports = new ProductsController();
--- src/routers/products/index.js
+++ src/routers/products/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
 const { asyncHandler } = require("../../commons/helps/asyncHandler");
-const { getAll } = require("../../controllers/product.controller");
+const { getAll, update } = require("../../controllers/product.controller");
 
 const router = express.Router();
 
 router.get("/getAll", asyncHandler(getAll));
+
+router.put("", asyncHandler(update));
 
 module.exports = router;
