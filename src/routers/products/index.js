3c1364b1b4d5b91d07fb9ad11588bfa0f541b274
Nguyen Tan <vantankmt@gmail.com>
2024-10-01 13:46:13 +0700

    feat: add verify token

--- src/routers/products/index.js
+++ src/routers/products/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const { asyncHandler } = require("../../commons/helps/asyncHandler");
 const {
@@ -8,9 +10,27 @@ const {
   deleteProduct,
 } = require("../../controllers/product.controller");
 
+function verifyToken(req, res, next) {
+  const token = req.headers['authorization'];
+
+  const tokenParts = token ? token.split(' ') : [];
+  if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
+    return res.status(403).send('Token is required');
+  }
+
+  jwt.verify(tokenParts[1], process.env.TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send('Invalid token');
+    }
+
+    req.user = decoded;
+    next();
+  });
+}
+
 const router = express.Router();
 
-router.get("/getAll", asyncHandler(getAll));
+router.get("/getAll", verifyToken, asyncHandler(getAll));
 
 router.put("/update", asyncHandler(update));
 
